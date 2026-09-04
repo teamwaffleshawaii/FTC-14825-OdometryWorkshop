9a6b183bd5d1b1be7aeea8723c1739215612c3d3
Guillermox808 <Guillermox808@gmail.com>
2026-09-04 08:09:46 -1000

    Added more Paths -GG

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,15 +9,27 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(700);
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 14.5)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 14.5)
                 .build();
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, Math.toRadians(90)))
 // Run your test paths here
 //...
 //Examples below
 //.strafeTo(new Vector2d(10, 0))
-.strafeToLinearHeading(new Vector2d(20,0), Math.toRadians(0))
-.strafeToLinearHeading(new Vector2d(-40,40), Math.toRadians(180))
+.waitSeconds(1.5)
+.strafeToLinearHeading(new Vector2d(-50, -50), Math.toRadians(235))
+.waitSeconds(3.0)
+.strafeToLinearHeading(new Vector2d(19, 32.5), Math.toRadians(0))
+.waitSeconds(3.0)
+.strafeToConstantHeading(new Vector2d(19, 13))
+.strafeToLinearHeading(new Pose2d(55, -15, Math.toRadians(0)).component1(), Math.toRadians(-180))
+.strafeToConstantHeading(new Vector2d(55, -33))
+.waitSeconds(3.0)
+.strafeToLinearHeading(new Vector2d(55, 38), Math.toRadians(90))
+.waitSeconds(3.0)
+.strafeToLinearHeading(new Vector2d(55, 0), Math.toRadians(270))
+.strafeToConstantHeading(new Vector2d(0, 0))
+.strafeToConstantHeading(new Vector2d(0, -45))
 //.splineTo(new Vector2d(0, 0), Math.toRadians(0))
 //.splineToConstantHeading(new Vector2d(0,0), Math.toRadians(0))
 //.splineToLinearHeading(new Pose2d(0,0, Math.toRadians(0)), Math.toRadians(0))
